b0262bd6155e963af1050afac878ee744294dc28
Irtaza64 <syedirtaza64@gmail.com>
2025-01-22 17:01:18 +0500

    committed up till part 4

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
+const config = require("config")
+// const dbgr = require("debug")("development:mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/scatch")
+// console.log(process.env.DEBUG);
+
+mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(function(){
     console.log("connected");
 })
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,5 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/ownersmodel")
+
+if(process.env.NODE_ENV === "development") {
+    router.post("/create", async (req, res) => {
+        let owners = await ownerModel.find();
+        if (owners.length > 0) {
+            return res.status(500).send("You cannot create a new owner,");
+        }
+
+        let { fullname, email, password } = req.body;
+
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password,
+        });
+
+        res.status(201).send(createdOwner)
+})
+}
 
 router.get("/", function (req, res) {
     res.send("hey its working");
